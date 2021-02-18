ab6876d2a504d5b882a7e3d9bef61e630ecd3369
underscore11code <minecrafter11mrt@gmail.com>
2021-02-17 21:34:09 -0800

    Initial Commit

---
+++ src/main/java/io/github/underscore11code/template/Template.java
@@ -0,0 +1,17 @@
+package io.github.underscore11code.template;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Template extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
